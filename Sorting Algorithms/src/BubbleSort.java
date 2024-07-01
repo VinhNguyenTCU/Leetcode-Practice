5934dd01c2f2a8386748749876dd34e168fe7597
Vinh Nguyen <vinh29012005@gmail.com>
2024-07-02 03:23:46 +0700

    selection and bubble sort algorithms

---
+++ Sorting Algorithms/src/BubbleSort.java
@@ -0,0 +1,40 @@
+public class BubbleSort {
+
+    /*
+     * How it works:
+     * 1. Go through the array, one value at a time
+     * 2. For each value, compare the value with next value
+     * 3. If the value is higher than the next one, swap the values so that the highest value comes last
+     * 4. Go through the array as many times as there are values in the array
+     */
+
+    public static void bubblesort(int[] nums){
+        int n = nums.length;
+        int temp;
+        boolean swapped;
+        do {
+            swapped = false;
+            for (int i = 0; i < n - 2; i++){
+                if (nums[i] > nums[i + 1]){
+                    temp = nums[i];
+                    nums[i] = nums[i + 1];
+                    nums[i + 1] = temp;
+                    swapped = true;
+                }
+            }
+        } while (swapped);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {64, 34, 25, 12, 22, 11, 90};
+        System.out.print("Unsorted array: ");
+        for (int num : nums){
+            System.out.print(" " + num);
+        }
+        bubblesort(nums);
+        System.out.print("\nSorted array used Bubble sort: ");
+        for (int num : nums){
+            System.out.print(" " + num);
+        }
+    }
+}
---
+++ Sorting Algorithms/src/SelectionSort.java
@@ -0,0 +1,41 @@
+public class SelectionSort {
+
+    /*Selection sort is a simple and efficient soring algorithm that works by repeatedly 
+     * selecting the smallest (or largest) element from the unsorted portion of the list and moving it to the sorted portion of the list.
+    */
+
+    /*
+     * How it works:
+     * 1. Go through the array to find the lowest value
+     * 2. Move the lowest value to the front of the unsorted part of the array
+     * 3. Go through the array again as many times as there are values in the array
+     */
+
+    // [64, 25, 12, 22, 11] --> [11, 12, 22, 25,64]
+    public static void selectionsort(int[] nums){
+        for (int i = 0; i < nums.length - 1; i++){
+            int min_index = i;
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[j] < nums[min_index]){
+                    min_index = j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[min_index];
+            nums[min_index] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {64, 25, 12, 22, 11};
+        System.out.print("Unsorted array: ");
+        for (int num : nums){
+            System.out.print(" " + num);
+        }
+        selectionsort(nums);
+        System.out.print("\nSorted array used Selection sort: ");
+        for (int num : nums){
+            System.out.print(" " + num);
+        }
+    }
+}
